2e8827ebc8b92d5c15eae37e7d0325d1bcf05a89
Cebonation <cebothando031@gmail.com>
2025-07-03 18:38:28 +0200

    Add files via upload

---
+++ Python/advanced_strings.py
@@ -0,0 +1,13 @@
+#Advanced concepts - Strings
+
+message = 'Helllo, World! '
+
+print(message.strip()) # Remove leading and trailing whitespace
+print(message.lower()) # Convert all characters to lowercase
+print(message.split(',')) # Split the string into a list based on the comma
+
+text = "hello world" 
+print(text.upper())
+
+text = " I like apple"
+print(text.replace("apples", "bananas"))
---
+++ Python/sets.py
@@ -0,0 +1,14 @@
+set1 = (1, 2, 3)
+set2 = (3, 4, 5)
+
+#Union
+union_set = set1.union9(set2)
+print(union_set)
+
+#Intersection
+inter_set = set1.intersection(set2)
+print(inter_set)
+
+#Different
+diff_set = set1.different(set2)
+print(diff_set)
